ce598dd6b77c42bbff4915ac42f50f52fec961bb
AnyaReese <2913678113@qq.com>
2024-04-14 16:58:39 +0800

    Reformat Code

--- proj3/garden.cpp
+++ proj3/garden.cpp
@@ -42,32 +42,8 @@ void restoreGarden(int PathIndex, int ValidPathNum, const vector<vector<Garden> 
     int Connector1_index;
     int Connector2_index;
 
-    // for(int i=0;i<TotalPathnum;++i)
-    // {
-    //     cout<<paths[i].valid<<" ";
-    // }
-    // cout<<"\n";
-
     // Check the validity of the fence placement
     if (ValidPathNum == PathNum) {
-
-//        bool flag = true;
-        // for (int i = 0; i < TotalPathnum; ++i) {
-        //     if (paths[i].valid) {
-        //         Connector1_index = garden[paths[i].begin.x][paths[i].begin.y].connector_index; // The Connector index of the begin of the path
-        //         Connector2_index = garden[paths[i].end.x][paths[i].end.y].connector_index;     // The Connector index of the end of the path
-        //         if (connectors[Connector1_index].degree + 1 > garden[paths[i].begin.x][paths[i].begin.y].degree ||
-        //             connectors[Connector2_index].degree + 1 > garden[paths[i].end.x][paths[i].end.y].degree) {
-        //             // cout<<"fail 1";
-        //             for (int j = 0; j < ConnectorNum; ++j) {
-        //                 connectors[j].degree = 0;
-        //             }
-        //             return;
-        //         }
-        //         connectors[Connector1_index].degree++;
-        //         connectors[Connector2_index].degree++;
-        //     }
-        // }
         
         for (int i = 0; i < ConnectorNum; ++i) {
             if (connectors[i].degree != garden[connectors[i].row][connectors[i].col].degree) {
@@ -120,7 +96,7 @@ void restoreGarden(int PathIndex, int ValidPathNum, const vector<vector<Garden> 
 
 int main() {
     int n, m; // Number of rows and columns in the garden.
-    vector<vector<Garden>> garden; // 2D array to store the garden layout.
+    vector<vector<Garden> > garden; // 2D array to store the garden layout.
     vector<Connector> connectors(3000); // Vector to store all connectors.
     vector<Path> paths(3000);
     int degree;
@@ -200,9 +176,6 @@ int main() {
         }
     }
 
-    // for (const auto& path : paths) {
-    //     std::cout << "Start: (" << path.begin.x << "," << path.begin.y << "), End:(" << path.end.x << "," << path.end.y << ")"<< endl;
-    // }
 /*---------------------------------------------------Restore the garden---------------------------------------------------------*/
 
     restoreGarden(0, 0, garden, paths, connectors); // Restore the garden.
